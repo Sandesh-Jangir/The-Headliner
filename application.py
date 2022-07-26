e2a88ead050d0c83f95976b6a8de6844b4e803a7
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-07-26 19:14:31 +0530

    Made The Data Dynamic

--- application.py
+++ application.py
@@ -1,3 +1,13 @@
 import eel
+import requests
+import json
+from api_key import key
 eel.init("web")
+@eel.expose
+def bbc_data():
+    url = f"https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={key}"
+    news = requests.get(url).text
+    news_py = json.loads(news)
+    articles = news_py['articles']
+    return articles
 eel.start("index.html")
